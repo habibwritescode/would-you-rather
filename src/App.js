257209835d12d823ff1468333bdd21f6d802b205
Habib Afolabi <habibwritescode@gmail.com>
2020-09-04 19:12:05 +0100

    Set up routes and add provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,72 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom'
+import { connect } from 'react-redux'
+import LoadingBar from 'react-redux-loading'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import PrivateRoute from './components/PrivateRoute'
+import Nav from './components/nav/Nav'
+import Signin from './components/signin/Signin'
+import HomePage from './pages/HomePage';
+import NewQuestion from './components/new-question/NewQuestion';
+import LeaderboardPage from './pages/LeaderboardPage';
+import Question from './components/question/Question';
+import Result from './components/result/Result';
+import { handleInitialData } from './redux/actions/shared';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <LoadingBar style={{ backgroundColor: 'blue' }} />
+        <div className="App">
+          <Nav />
+          {this.props.loading === true
+            ? null
+            :
+            <div>
+              <Switch>
+                <PrivateRoute exact path='/'>
+                  <HomePage />
+                </PrivateRoute>
+                <PrivateRoute path='/add'>
+                  <NewQuestion />
+                </PrivateRoute>
+                <PrivateRoute path='/leaderboard'>
+                  <LeaderboardPage />
+                </PrivateRoute>
+                <PrivateRoute path='/question/:id'>
+                  <Question />
+                </PrivateRoute>
+                <PrivateRoute path='/result/:id'>
+                  <Result />
+                </PrivateRoute>
+                <Route path='/signin' render={() =>
+                  this.props.authedUser ? (
+                    <Redirect to='/' />
+                  ) : (
+                      <Signin />
+                    )
+                } />
+              </Switch>
+            </div>
+          }
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps({ authedUser, loadingBar }) {
+  return {
+    authedUser,
+    loading: loadingBar.default === 1,
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
 import App from './App';
+import store from './redux/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
