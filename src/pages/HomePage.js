6482a2e6bd5fefc1eca5ce25254623a2626f47df
Habib Afolabi <habibwritescode@gmail.com>
2020-09-04 19:02:39 +0100

    Add pages

---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import PollList from '../components/poll-list/PollList';
+
+function HomePage() {
+    return <PollList />
+}
+
+export default HomePage
---
+++ src/pages/LeaderboardPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+import LeaderBoardCard from '../components/leader-board-card/LeaderBoardCard';
+
+
+function LeaderBoardPage() {
+    return (
+        <ul>
+            {this.props.sortIds.map(id => (
+                <LeaderBoardCard key={id} id={id} />
+            ))}
+        </ul>
+    )
+}
+
+function mapStateToProps({ users }) {
+    const sortIds = Object.keys(users).sort((a, b) => (Object.keys(users[b].answers).length + users[b].questions.length) - (Object.keys(users[a].answers).length + users[a].questions.length))
+    return {
+        sortIds
+    }
+}
+
+export default connect(mapStateToProps)(LeaderBoardPage)
