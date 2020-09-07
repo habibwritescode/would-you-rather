4405a24d672136d914ddda23fa00a7e8355623cd
Habib Afolabi <habibwritescode@gmail.com>
2020-09-07 18:48:10 +0100

    Create 404 error page

---
+++ src/components/NoMatch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function NoMatch() {
+
+    return (
+        <div className='container'>
+            <h3>
+                This page doesn't exist
+            </h3>
+        </div>
+    );
+}
