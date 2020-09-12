4b35507a8ec66752da7814c22fd1ec6259752ca9
Habib Afolabi <habibwritescode@gmail.com>
2020-09-12 10:24:12 +0100

    fix: Add redux thunk for production build

--- src/redux/middleware/middleware.js
+++ src/redux/middleware/middleware.js
@@ -2,10 +2,10 @@ import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import { applyMiddleware } from 'redux'
 
-const middlewares = []
+const middlewares = [thunk]
 
-if(process.env.NODE_ENV === 'development'){
-    middlewares.push(thunk, logger)
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger)
 }
 
 export default applyMiddleware(...middlewares)
